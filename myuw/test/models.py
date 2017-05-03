2271f207d68df9ec3efa52f82e22f7c4bea62244
Ethan Turner <ezturner@washington.edu>
2017-05-03 12:28:06 -0700

    Added ResCategoryLinks model tests

--- myuw/test/models.py
+++ myuw/test/models.py
@@ -12,3 +12,60 @@ class TestUserNotices(TestCase):
         model = UserNotices()
         hash = model.generate_hash(notice)
         self.assertEquals(hash, "fbb05e8edaf403b7c5eb5a9ac8d7cc3b")
+
+
+class TestResCategoryLinks(TestCase):
+
+    def test_affiliation(self):
+        """
+        Tests the affiliation format, where the affiliation tags are
+        separated by '+' characters and the ugrad/grad/
+        """
+        link = ResCategoryLink(
+            url="http://www.washington.edu/students/reg/calendar.html",
+            title="Academic Calendar",
+            affiliation="all+",
+            category_name="Academics",
+            sub_category="Registration",
+            new_tab="yes"
+            )
+
+        self.assertEquals(link.get_affiliation(), "all")
+        self.assertEquals(link.get_pce(), "")
+
+        link = ResCategoryLink(
+            url="http://www.washington.edu/students/reg/calendar.html",
+            title="Academic Calendar",
+            affiliation="ugrad+",
+            category_name="Academics",
+            sub_category="Registration",
+            new_tab="yes"
+            )
+
+        self.assertEquals(link.get_affiliation(), "ugrad")
+        self.assertEquals(link.get_pce(), "")
+
+        link = ResCategoryLink(
+            url="http://www.washington.edu/students/reg/calendar.html",
+            title="Academic Calendar",
+            affiliation="nm+",
+            category_name="Academics",
+            sub_category="Registration",
+            new_tab="yes"
+            )
+
+        self.assertEquals(link.get_affiliation(), "nm")
+        self.assertEquals(link.get_pce(), "")
+
+    def test_pce(self):
+        link = ResCategoryLink(
+            url="http://www.washington.edu/students/reg/calendar.html",
+            title="Academic Calendar",
+            affiliation="all+pce",
+            category_name="Academics",
+            sub_category="Registration",
+            new_tab="yes"
+            )
+
+        self.assertEquals(link.get_affiliation(), "all")
+        self.assertEquals(link.get_pce(), "pce")
